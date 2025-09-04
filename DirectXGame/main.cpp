3e18ee09b4216556fc75a7ab7dddd9144f130408
SaitoYuuki48 <115055003+SaitoYuuki48@users.noreply.github.com>
2025-09-04 15:17:23 +0900

    KamataEngineを入れた

---
+++ DirectXGame/main.cpp
@@ -0,0 +1,49 @@
+#include <Windows.h>
+#include "KamataEngine.h"
+#include "GameScene.h"
+
+using namespace KamataEngine;
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
+	
+	// DirectXCommonインスタンスの取得
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+	//エンジンの初期化
+	KamataEngine::Initialize(L"4047_破断率");
+
+	//ゲームシーンのインスタンス生成
+	GameScene* gameScene = new GameScene();
+	//ゲームシーンの初期化
+	gameScene->Initialize();
+
+	//メインループ
+	while (true) {
+		// エンジンの更新
+		if (KamataEngine::Update()) {
+			break;
+		}
+
+		//ゲームシーンの更新
+
+		//描画開始
+		dxCommon->PreDraw();
+
+		//ここに描画処理を記述する
+		// ゲームシーンの描画
+		gameScene->Draw();
+
+		// 描画終了
+		dxCommon->PostDraw();
+	}
+
+	//ゲームシーンの解放
+	delete gameScene;
+	// nullptrの代入
+	gameScene = nullptr;
+
+	//エンジンの修了処理
+	KamataEngine::Finalize();
+
+	return 0;
+}

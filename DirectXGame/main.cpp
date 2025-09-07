3c2090abfdce67fe1fbef183b54d922d670411a6
watanabe-akihiro <k022g0118@g.neec.ac.jp>
2025-09-07 15:45:31 +0900

    渾身のプログラム(バグってる)

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -1,52 +1,102 @@
-#include <Windows.h>
-#include "KamataEngine.h"
+//#include <Windows.h>
+//#include "KamataEngine.h"
+//#include "GameScene.h"
+//
+//using namespace KamataEngine;
+//
+//// Windowsアプリでのエントリーポイント(main関数)
+//int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
+//	
+//	// DirectXCommonインスタンスの取得
+//	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+//	//エンジンの初期化
+//	KamataEngine::Initialize(L"4047_破断率");
+//
+//	//ゲームシーンのインスタンス生成
+//	GameScene* gameScene = new GameScene();
+//	//ゲームシーンの初期化
+//	gameScene->Initialize();
+//	
+//	//メインループ
+//	while (true) {
+//		// エンジンの更新
+//		if (KamataEngine::Update()) {
+//			break;
+//		}
+//
+//		//ゲームシーンの更新
+//
+//		//描画開始
+//		dxCommon->PreDraw();
+//
+//		//ここに描画処理を記述する
+//		
+//		//スプライト描画前処理
+//		Sprite::PreDraw();
+//		
+//		// ゲームシーンの描画
+//		gameScene->Draw();
+//
+//		// 描画終了
+//		dxCommon->PostDraw();
+//	}
+//
+//	//ゲームシーンの解放
+//	delete gameScene;
+//	// nullptrの代入
+//	gameScene = nullptr;
+//
+//	//エンジンの修了処理
+//	KamataEngine::Finalize();
+//
+//	return 0;
+//}
+
 #include "GameScene.h"
+#include "KamataEngine.h"
+#include <Windows.h>
 
 using namespace KamataEngine;
 
-// Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
-	
-	// DirectXCommonインスタンスの取得
+
 	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
-	//エンジンの初期化
+
+	// エンジン初期化
 	KamataEngine::Initialize(L"4047_破断率");
 
-	//ゲームシーンのインスタンス生成
+	// GameScene インスタンス生成
 	GameScene* gameScene = new GameScene();
-	//ゲームシーンの初期化
 	gameScene->Initialize();
-	
-	//メインループ
+
+	// メインループ
 	while (true) {
-		// エンジンの更新
+
+		// エンジン更新
 		if (KamataEngine::Update()) {
 			break;
 		}
 
-		//ゲームシーンの更新
+		// GameScene 更新
+		gameScene->Update();
 
-		//描画開始
+		// 描画開始
 		dxCommon->PreDraw();
+		Sprite::PreDraw(dxCommon->GetCommandList());
 
-		//ここに描画処理を記述する
-		
-		//スプライト描画前処理
-		Sprite::PreDraw();
-		
-		// ゲームシーンの描画
+		// GameScene 描画
 		gameScene->Draw();
 
 		// 描画終了
+		Sprite::PostDraw();
 		dxCommon->PostDraw();
 	}
 
-	//ゲームシーンの解放
+	// GameScene 解放
 	delete gameScene;
-	// nullptrの代入
 	gameScene = nullptr;
 
-	//エンジンの修了処理
+	// エンジン終了
 	KamataEngine::Finalize();
 
 	return 0;
